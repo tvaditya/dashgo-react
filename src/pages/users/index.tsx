2ceba68f8bad724dbed0a271d1c01cce55801657
tvaditya <tvaditya@gmail.com>
2021-07-05 16:34:30 -0300

    User list factory (miragejs) created

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,12 +19,19 @@ import {Pagination} from "../../components/Pagination";
 import {Sidebar} from "../../components/Sidebar";
 import {Header} from "../../components/Header";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useEffect } from "react";
 
 export default function UserList() {
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, [])
     
     return (
         <Box>

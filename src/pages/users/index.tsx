a0896511bbcc95afad339f0cfc4bc8539d1154fd
tvaditya <tvaditya@gmail.com>
2021-07-05 17:42:10 -0300

    List of users created with mirage and react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -26,9 +26,21 @@ import { useQuery} from "react-query";
 export default function UserList() {
     const { data, isLoading, error} = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users');
-        const data = await response.json();
+        const data = await response.json()
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric',
+                })
+            }
+        })
+        return users;
     })
 
     // console.log(query)
@@ -85,29 +97,33 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme={"pink"} />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight={"bold"}>Tantravahi Aditya</Text>
-                                                <Text fontSize={"sm"} color={"gray.300"}>tvaditya@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        { isWideVersion && <Td>06 de Junho, 2021</Td> }
-                                        <Td>
-                                            <Button
-                                                as={"a"}
-                                                size={"sm"}
-                                                fontSize={"sm"}
-                                                colorScheme={"purple"}
-                                                leftIcon={<Icon as={RiPencilLine} fontSize={"16"} />}
-                                            >
-                                                { isWideVersion ? 'Editar' : ''}
-                                            </Button>
-                                        </Td>
-                                    </Tr>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme={"pink"} />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight={"bold"}>{user.name}</Text>
+                                                        <Text fontSize={"sm"} color={"gray.300"}>{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion && <Td>{user.createdAt}</Td> }
+                                                {/*<Td>*/}
+                                                {/*    <Button*/}
+                                                {/*        as={"a"}*/}
+                                                {/*        size={"sm"}*/}
+                                                {/*        fontSize={"sm"}*/}
+                                                {/*        colorScheme={"purple"}*/}
+                                                {/*        leftIcon={<Icon as={RiPencilLine} fontSize={"16"} />}*/}
+                                                {/*    >*/}
+                                                {/*        { isWideVersion ? 'Editar' : ''}*/}
+                                                {/*    </Button>*/}
+                                                {/*</Td>*/}
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
                             <Pagination />

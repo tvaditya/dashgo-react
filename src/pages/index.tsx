505b10dff63ccacf87fe4449bb57f230694d12e3
tvaditya <tvaditya@gmail.com>
2021-07-04 23:10:25 -0300

    Field validation created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import {Flex, Button, Stack} from "@chakra-ui/react";
 import { Input } from '../components/Form/Input';
 import {SubmitHandler, useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type SignInFormData = {
     email: string;
     password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+    email: yup.string().required('Email obrigatorio').email('Email: formato invalido'),
+    password: yup.string().required('Senha obrigatoria'),
+})
+
 export default function SignIn() {
-    const { register, handleSubmit, formState} = useForm()
+    const { register, handleSubmit, formState} = useForm({
+        resolver: yupResolver(signInFormSchema)
+    })
+
+    const { errors } = formState
+
+    console.log(errors)
 
     const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
         await new Promise(resolve => setTimeout(resolve, 1555))
@@ -33,8 +46,20 @@ export default function SignIn() {
             onSubmit={handleSubmit(handleSignIn)}
         >
             <Stack spacing={"4"}>
-                <Input name={"email"} type={"email"} label={"E-mail"} {...register('email')}/>
-                <Input name={"password"} type={"password"} label={"Password"} {...register('password')} />
+                <Input
+                    name={"email"}
+                    type={"email"}
+                    label={"E-mail"}
+                    error={errors.email}
+                    {...register('email')}
+                />
+                <Input
+                    name={"password"}
+                    type={"password"}
+                    label={"Password"}
+                    error={errors.password}
+                    {...register('password')}
+                />
             </Stack>
 
             <Button
@@ -50,3 +75,5 @@ export default function SignIn() {
       </Flex>
   )
 }
+
+

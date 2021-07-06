5fa7276f41c5c2fa948e94ce5a57a96ace24bf9d
tvaditya <tvaditya@gmail.com>
2021-07-06 10:34:26 -0300

    Axios configured, replacing fetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,11 +22,12 @@ import {Header} from "../../components/Header";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 // import { useEffect } from "react";
 import { useQuery} from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
     const { data, isLoading, isFetching, error} = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users');
-        const data = await response.json()
+        const { data } = await api.get('users');
+   //     const data = await response.json() ** with fetch
 
         const users = data.users.map(user => {
             return {

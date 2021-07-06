b72384057386776d0ec11961435cd35d401e8c27
tvaditya <tvaditya@gmail.com>
2021-07-06 11:12:22 -0300

    List of user hook created

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,30 +21,10 @@ import {Sidebar} from "../../components/Sidebar";
 import {Header} from "../../components/Header";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 // import { useEffect } from "react";
-import { useQuery} from "react-query";
-import { api } from "../../services/api";
+import {useUsers} from "../../services/hooks/useUsers";
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error} = useQuery('users', async () => {
-        const { data } = await api.get('users');
-   //     const data = await response.json() ** with fetch
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                })
-            }
-        })
-        return users;
-    },{
-        staleTime: 1000 * 3,
-    })
+    const { data, isLoading, isFetching, error} = useUsers()
 
     // console.log(query)
 
